2ac563eb82a3a39d76587f9dc3a3339140e683d9
Aevarkan <aevarkan@gmail.com>
2025-10-25 20:55:56 +0800

    docs(src): added docstrings to functions

--- src/functions.ts
+++ src/functions.ts
@@ -19,27 +19,82 @@ import { CommandInfo, CommandParameterInfo, CommandParameterInfoEnum, CommandPar
 
 /**
  * Defines an `Enum` command parameter.
- * @param param The command parameter object.
+ * 
+ * `Enum` parameters have an extra `values` property that must not be empty.
+ * 
+ * @param parameter The command parameter object.
+ * 
+ * @example fillCommand.ts
+ * ```ts
+ * const fillType = defineParameter({
+ *     name: "mode",
+ *     type: CustomCommandParamType.Enum,
+ *     mandatory: true,
+ *     values: ["default", "hollow", "walls"]
+ * });
+ * ```
  */
 export function defineParameter<V extends readonly string[]>(
-    param: CommandParameterInfoEnum<V>
+    parameter: CommandParameterInfoEnum<V>
 ): CommandParameterInfoEnum<V>;
 
 /**
- * Defines a generic custom command parameter. 
- * @param param The command parameter object.
+ * Defines a generic custom command parameter.
+ * 
+ * @param parameter The command parameter object.
+ * 
+ * @example moneyCommand.ts
+ * ```ts
+ * const allowDebt = defineParameter({
+ *     name: "allowDebt",
+ *     type: CustomCommandParamType.Boolean,
+ *     mandatory: false,
+ * });
+ * ```
  */
 export function defineParameter<T extends Exclude<CustomCommandParamType, CustomCommandParamType.Enum>>(
-    param: CommandParameterInfoGeneric<T>
+    parameter: CommandParameterInfoGeneric<T>
 ): CommandParameterInfoGeneric<T>;
 
 export function defineParameter<
     P extends CommandParameterInfo
->(param: P): P {
-    return param
+>(parameter: P): P {
+    return parameter
 }
 
-
+/**
+ * Define a custom command.
+ * 
+ * @param command The {@link CommandInfo} object.
+ * 
+ * @throws Optional `parameters` **MUST** come after mandatory `parameters` in the array, a runtime error will be thrown otherwise.
+ * 
+ * @example chanceCommand.ts
+ * ```typescript
+ * const percentageChanceParameter = defineParameter({
+ *     name: "percentageChance",
+ *     type: CustomCommandParamType.Integer,
+ *     mandatory: true
+ * })
+ *  
+ * const commandParam = defineParameter({
+ *     name: "command",
+ *     type: CustomCommandParamType.String,
+ *     mandatory: true
+ * })
+ *  
+ * const chanceCommand = defineCommand({
+ *     name: "chance",
+ *     description: "Runs a command randomly, according to if generated number is less than percentage chance.",
+ *     permissionLevel: CommandPermissionLevel.GameDirectors,
+ *     cheatsRequired: true,
+ *     parameters: [percentageChanceParameter, commandParam],
+ *     callbackFunction: (origin, chance, command) => {
+ *         // chance command function
+ *     }
+ * })
+ * 
+ */
 export function defineCommand<const P extends CommandParameterInfo[]>(
     command: CommandInfo<P>
 ): CommandInfo<P> {
